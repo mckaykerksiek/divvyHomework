fa6dedde0c3ee1b1e7ac1d1c97a3125057221e04
McKay Michaelis <mckaykerksiek@gmaillcom>
2021-11-18 12:10:43 -0700

    add functionality to read in json file and display companies in list

--- screens/Businesses.js
+++ screens/Businesses.js
@@ -1,9 +1,35 @@
-import * as React from 'react';
-import { FlatList, Text } from 'react-native';
+import  React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { FlatList, Text, StyleSheet, View } from 'react-native';
+
+const companiesData = require('../data.json');
 
 export default class Businesses extends React.Component {
+  state = {isLoading: false}
+  
+
   render() {
-    return <Text>Foo bar shibby</Text>
+    return (
+    <View style={styles.container}>
+      <FlatList
+        data={companiesData}
+        keyExtractor={({ id }) => id.toString()}
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+      />
+    </View>
+    )
     // return <FlatList />
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
