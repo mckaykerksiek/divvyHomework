4577b63e7b30ad210f4335de2e8978f54e30447f
McKay Michaelis <mckaykerksiek@gmaillcom>
2021-11-18 21:55:01 -0700

    add dummy data to chart to make it show up

--- ios/RNTChartManager.m
+++ ios/RNTChartManager.m
@@ -21,9 +21,66 @@
 
 RCT_EXPORT_MODULE(RNTChart)
 
+
+
 - (UIView *)view
 {
-  return [[BarChartView.class alloc] init];
+//  RNTMapView *map = [RNTMapView new];
+//    map.delegate = self;
+//    return map;
+  BarChartView *chart = [BarChartView new];
+  return [self setDataCount:6 range:20];
 }
+
+- (BarChartView *)setDataCount:(int)count range:(double)range
+{
+//    {
+//        "seq": 0,
+//        "date": "2019-03-12 00:14:10",
+//        "value": 77793740.07
+//    },
+//    {
+//        "seq": 1,
+//        "date": "2019-02-12 00:14:10",
+//        "value": 17659305.4
+//    }
+    BarChartView *chartView = [BarChartView new];
+    NSMutableArray *yVals = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < count; i++)
+    {
+//        double mult = (range + 1);
+//        double val = (double) (arc4random_uniform(mult)) + mult / 3.0;
+      double val = (double) 77793740.07;
+        [yVals addObject:[[BarChartDataEntry alloc] initWithX:i y:val]];
+    }
+    
+    BarChartDataSet *set1 = nil;
+    if (chartView.data.dataSetCount > 0)
+    {
+        set1 = (BarChartDataSet *)chartView.data.dataSets[0];
+        [set1 replaceEntries:yVals];
+        [chartView.data notifyDataChanged];
+        [chartView notifyDataSetChanged];
+    }
+    else
+    {
+        set1 = [[BarChartDataSet alloc] initWithEntries:yVals label:@"DataSet"];
+        set1.colors = ChartColorTemplates.vordiplom;
+        set1.drawValuesEnabled = NO;
+        
+        NSMutableArray *dataSets = [[NSMutableArray alloc] init];
+        [dataSets addObject:set1];
+        
+        BarChartData *data = [[BarChartData alloc] initWithDataSets:dataSets];
+        
+        chartView.data = data;
+        chartView.fitBars = YES;
+    }
+    
+    [chartView setNeedsDisplay];
+    return chartView;
+}
+
 
 @end
--- screens/ChartView.js
+++ screens/ChartView.js
@@ -1,4 +1,17 @@
 import { requireNativeComponent } from 'react-native';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-// requireNativeComponent automatically resolves 'RNTMap' to 'RNTMapManager'
-module.exports = requireNativeComponent('RNTChart');
+class ChartView extends React.Component {
+    render() {
+        return <RNTChart {...this.props} />
+    }
+}
+
+ChartView.propTypes = {
+    
+}
+
+// requireNativeComponent automatically resolves 'RNTChart' to 'RNTChartManager'
+var RNTChart = requireNativeComponent('RNTChart');
+module.exports = ChartView
